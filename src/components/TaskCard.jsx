1286e4a7e7ac948dd7bd211c846a3e6161959dcf
Minu <minu.kim2003@gmail.com>
2021-10-20 15:12:52 +0200

    add All Tasks Card

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,3 +1,11 @@
-export const TaskCard = () => {
-  return <div></div>;
+export const TaskCard = (props) => {
+  return (
+    <div className="taskCard">
+      <p className="taskName">{props.name}</p>
+      <img src={props.img} />
+      <p className="taskTitle">{props.title}</p>
+      <p className="taskDescription">{props.description}</p>
+      <p className="taskDue">{props.due}</p>
+    </div>
+  );
 };
--- src/pages/AllTasksPage.jsx
+++ src/pages/AllTasksPage.jsx
@@ -4,30 +4,56 @@ import { TaskCard } from "../components/TaskCard";
 export const AllTasksPage = () => {
   const allTasks = [
     {
+      id: 1,
       name: "Minu Kim",
       profile_pic:
-        "https://de.wikipedia.org/wiki/Kim_Jong-un#/media/Datei:Kim_Jong-un_April_2019_(cropped).jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/6/6e/Kim_Jong-un_April_2019_%28cropped%29.jpg",
       title: "Finish Poke-API",
       description: "Finalize styling the Pokedex-Website",
       due: "Tuesday",
     },
     {
+      id: 2,
       name: "Isaac Newton",
       profile_pic:
-        "https://en.wikipedia.org/wiki/Isaac_Newton#/media/File:Portrait_of_Sir_Isaac_Newton,_1689.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/3/3b/Portrait_of_Sir_Isaac_Newton%2C_1689.jpg",
       title: "Invent new stuff",
       description:
         "Come up with unnecessary shit, so Minu has a harder time in school",
       due: "Friday",
     },
     {
+      id: 3,
       name: "Isaac Tshilumba",
       profile_pic: "https://i.ytimg.com/vi/ZxhpXG9crE8/hqdefault.jpg",
       title: "League",
       description: "Forget school and come to discord and play with bff Minu",
       due: "Everyday",
     },
+    // {
+    //   id: 4,
+    //   name: "lorem lorem",
+    //   title: "lorem",
+    //   description:
+    //     " Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae iste commodi voluptate suscipit esse. Quo dolore qui reiciendis, perspiciatis hic recusandae quod dignissimos quasi dolorem id nobis. Odio voluptatum iure nostrum esse unde ad sapiente, nesciunt dignissimos distinctio consequuntur maxime autem optio atque. Voluptates hic ipsum a totam? Beatae, nam.",
+    //   due: "Sunday",
+    // },
   ];
 
-  return <div></div>;
+  return (
+    <div className="allTasksPage">
+      {allTasks.map((e) => {
+        return (
+          <TaskCard
+            name={e.name}
+            img={e.profile_pic}
+            title={e.title}
+            description={e.description}
+            due={e.due}
+            key={e.id}
+          />
+        );
+      })}
+    </div>
+  );
 };
